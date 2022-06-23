b3af31184414719d7f2e32eff7bacfdea6f6dcd6
Angel Concepcion <angelconcepcion50@gmail.com>
2022-06-23 14:58:07 -0400

    feature: working solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // your code here
+        System.out.println("Please enter your age:");
+        Scanner inputScanner = new Scanner(System.in);
+        int userAge = inputScanner.nextInt();
+        if(userAge > 18){
+            System.out.println("Congrats you are an adult!");
+        }
+        else if (userAge < 18 && userAge > 10){
+            System.out.println("Youre basically a teenager I'm not sure yet");
+        }
+        else {
+            System.out.println("Hey buddy how are you?");
+        } ;
+         
     }
 }
